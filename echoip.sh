662fc580615a1ff96dcc1bfc89292acbbd49a7d0
Martazza <Martazza@users.noreply.github.com>
2020-02-29 19:06:21 +0100

    Add echoip

---
+++ echoip.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Echoip v1.0
+# Author: @Martazza
+# https://github.com/Martazza/echoip
+# Give me the credits if you use any part of this code, read the License.
+
+wget https://reyboz.it/ip.php
